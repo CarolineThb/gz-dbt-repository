42f47cb734e849b23f7e42413b402f725a27dcb7
Caroline THIBAULT <caroline.th38@gmail.com>
2023-11-15 14:27:04 +0000

    create custom functions margin percent

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,6 +5,8 @@ select
     revenue,
     quantity,
     round((s.quantity * p.purchase_price), 2) as purchase_cost,
-    round(s.revenue - (s.quantity * p.purchase_price), 2) as margin
-        from {{ ref("stg_raw__sales") }} s
-        join {{ ref("stg_raw__product") }} p using (products_id)
+    round(s.revenue - (s.quantity * p.purchase_price), 2) as margin,
+    {{ margin_percent('revenue' , 's.quantity*p.purchase_price') }} *100 as margin_percent
+from {{ ref("stg_raw__sales") }} s
+join {{ ref("stg_raw__product") }} p
+    using (products_id)
